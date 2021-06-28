f5b5354f9bd42b820bd5712c0e1743e1ec6ba1d9
codigen <ile.bright001@gmail.com>
2021-06-28 19:18:41 +0100

    data changes

--- routes/employee.js
+++ routes/employee.js
@@ -39,9 +39,7 @@ router.get("/getRecords", async(req, res) => {
 
     res.status(200).json({
         success: true,
-        data:{
-            employeeData
-        },
+        employeeData,
         noOfEmployees: employeeData.data.length
     })
 
